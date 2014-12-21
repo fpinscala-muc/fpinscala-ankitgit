cb640b25ff0b8d6bfd2e35450fbcbe5481720fa4
Ankit Chaudhary <my007id@gmail.com>
2014-12-21 12:43:18 +0100

    Tail and Head
    All thanks to Cons

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -46,10 +46,17 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(ns: List[Double]) = 
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  def tail[A](l: List[A]): List[A] =
+    l match {
+//      case Nil => Nil
+      case Cons(x, xs) => xs
+    }
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
-
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match {
+//      case Nil => Nil
+      case Cons(x, xs) => Cons(h, xs)
+    }
 
   def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
 
